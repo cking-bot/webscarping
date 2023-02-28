9433345e30e2f8040abf7ab031f84e2eae947c94
itscharlzard <kingcharlie1103@yahoo.com>
2023-02-28 11:18:38 -0800

    added requirement file

---
+++ mapit.py
@@ -0,0 +1,13 @@
+"""mapit.py
+
+Launch a map in the browser using an address from the command line or clipboard.
+"""
+
+import webbrowser
+import sys
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = " ".join(sys.argv[1:])
+
+# TODO: Get address from clipboard.
